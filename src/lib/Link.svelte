5f63abb60f0cb2dd791889a1e1a1cd0d786cd4ca
Lucas McClean <lucksdeveloper@gmail.com>
2025-09-28 01:42:34 -0400

    altimate showcase

---
+++ src/lib/Link.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import LinkArrow from '$lib/internal/LinkArrow.svelte';
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+	const {
+		children,
+		external = false,
+		target,
+		rel,
+		class: className = '',
+		...rest
+	}: HTMLAnchorAttributes & {
+		external?: boolean;
+		target?: string;
+		rel?: string;
+		class?: string;
+		children: () => unknown;
+	} = $props();
+
+	const computedTarget = external ? '_blank' : target;
+	const computedRel = external ? (rel ?? 'noopener noreferrer') : rel;
+</script>
+
+<a
+	{...rest}
+	target={computedTarget}
+	rel={computedRel}
+	class={`relative inline items-center gap-1 transition-colors
+          after:absolute after:bottom-[0px] after:left-0 after:h-[2px] after:w-0
+          after:bg-current after:transition-all
+          hover:after:w-full focus-visible:after:w-full ${className}`}
+>
+	{@render children()}
+	{#if external}
+		<LinkArrow />
+	{/if}
+</a>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '/logo.png';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href="/favicon.png" />
+</svelte:head>
+
+<div>
+	<a href="#main-content" class="sr-only focus:not-sr-only">Skip to main content</a>
+	{@render children?.()}
+</div>
